d65416e72c111a123a4f1b517293a9e5243227db
Forest Newark <forest.newark@gmail.com>
2017-03-04 12:56:58 -0500

    Refactored both classes

--- src/Main.java
+++ src/Main.java
@@ -8,56 +8,25 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Enter your message: ");
-        int shiftNumber = 2;
+
         Scanner sc = new Scanner(System.in);
         Message message = new Message();
+        message.shiftNumber = 2;
         message.messageToEncrypt = sc.nextLine();
+
+
         System.out.println("Your Message to Encrypt is: " + message.messageToEncrypt);
+        message.encodeMessage();
+        System.out.println("Encrypted Message is: " + message.eMessage);
+        System.out.println("Your Cipher key is: " + message.shiftNumber);
 
-        //Turn String into Char Array
-        char[] charArray = message.messageToEncrypt.toCharArray();
+        System.out.println("Now Decoding Message");
+        System.out.println("......");
+        System.out.println("......");
+        System.out.println("Your Decoded Message is: " + message.decodeMessage());
 
-       //Turn Char Array into int Array (called numbArray). Add ShiftNumber
-        int[] numbArray = new int[charArray.length];
-        for (int count = 0; count < charArray.length; count++) {
-            int digit = charArray[count];
-            numbArray[count] = digit + shiftNumber;
 
-        }
 
-        //Turn Shifted Number Array back to Char Array (ENCODED)
-        char[] encodeCharArray = new char[numbArray.length];
-        for (int count=0; count < numbArray.length; count++) {
-            char letter = (char) numbArray[count];
-            encodeCharArray[count] = letter;
-
-        }
-
-        String encodedMessage = String.valueOf(encodeCharArray);
-        System.out.println("Your Encoded Message is: " +encodedMessage);
-        System.out.println("Your Cipher Key is: " + shiftNumber);
-
-        //encoded Message to Char Array
-        char[] decodeCharArray = encodedMessage.toCharArray();
-
-        //Turn Char Array (encoded) into int Array (called decodeNumbArray). SUBTRACT ShiftNumber
-        int[] decodeNumbArray = new int[decodeCharArray.length];
-        for (int count = 0; count < decodeCharArray.length; count++) {
-            int digit = decodeCharArray[count];
-            decodeNumbArray[count] = digit - shiftNumber;
-
-        }
-
-        //Turn UN-Shifted Number Array back to Char Array (DECODED)
-        char[] decodedCharArray = new char[decodeNumbArray.length];
-        for (int count=0; count < decodeNumbArray.length; count++) {
-            char letter = (char) decodeNumbArray[count];
-            decodedCharArray[count] = letter;
-
-        }
-
-        String decodedMessage = String.valueOf(decodedCharArray);
-        System.out.println("Your Decoded Message is: " + decodedMessage);
 
 
 
--- src/Message.java
+++ src/Message.java
@@ -3,5 +3,66 @@
  */
 public class Message {
     String messageToEncrypt;
+     String eMessage;
+     String dMessage;
     int shiftNumber;
+
+    //Encode Method
+    public String encodeMessage() {
+
+
+        //Turn String into Char Array
+        char[] toEncodeCharArray = messageToEncrypt.toCharArray();
+
+        //Turn Char Array into int Array (called numbArray). Add ShiftNumber
+        int[] encodeNumberArray = new int[toEncodeCharArray.length];
+        for (int count = 0; count < toEncodeCharArray.length; count++) {
+            int digit = toEncodeCharArray[count];
+            encodeNumberArray[count] = digit + shiftNumber;
+
+        }
+
+        //Turn Shifted Number Array back to Char Array (ENCODED)
+        char[] encodedCharArray = new char[encodeNumberArray.length];
+        for (int count=0; count < encodeNumberArray.length; count++) {
+            char letter = (char) encodeNumberArray[count];
+            encodedCharArray[count] = letter;
+
+        }
+
+        this.eMessage = String.valueOf(encodedCharArray);
+        return eMessage;
+    }
+
+    //Decode Method
+    public String decodeMessage() {
+
+        //encoded Message to Char Array
+        char[] toDecodeCharArray = this.eMessage.toCharArray();
+
+        //Turn Char Array (encoded) into int Array (called decodeNumbArray). SUBTRACT ShiftNumber
+        int[] toDecodeNumberArray = new int[toDecodeCharArray.length];
+        for (int count = 0; count < toDecodeCharArray.length; count++) {
+            int digit = toDecodeCharArray[count];
+            toDecodeNumberArray[count] = digit - shiftNumber;
+
+        }
+
+        //Turn UN-Shifted Number Array back to Char Array (DECODED)
+        char[] decodedCharArray = new char[toDecodeNumberArray.length];
+        for (int count=0; count < toDecodeNumberArray.length; count++) {
+            char letter = (char) toDecodeNumberArray[count];
+            decodedCharArray[count] = letter;
+
+        }
+
+        String decodedMessage = String.valueOf(decodedCharArray);
+        this.dMessage = decodedMessage;
+        return dMessage;
+    }
+
+
+
+
+
 }
